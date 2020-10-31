1c44b6b3caeaf73e271d52c0ab8ec68baeb0c588
DanielVillalba-holberton <1821@holbertonschool.com>
2020-10-31 22:47:45 +0000

    added do_create to console

--- console.py
+++ console.py
@@ -2,10 +2,13 @@
 """ Command interpreter """
 
 import cmd
-
+import models
+from models.base_model import BaseModel
+storage = models.storage
 
 class HBNBCommand(cmd.Cmd):
     """ Command interpreter """
+
     def __init__(self):
         """ Instatiation for class HBNB """
         cmd.Cmd.__init__(self)
@@ -21,6 +24,23 @@ class HBNBCommand(cmd.Cmd):
         """ Signal to interrupt a file """
         return True
 
+    def do_create(self, args):
+        """reads a line from command prompt and create an instance"""
+
+        dict_class = {'BaseModel': BaseModel}
+
+        if args == '':
+            print('** class name missing **')
+        
+        elif args != 'BaseModel':
+            print("** class doesn't exist **")
+
+        else:
+            for key, value in dict_class.items():
+                if key == args:
+                    object_d = value()
+                    object_d.save()
+                    print(object_d.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
