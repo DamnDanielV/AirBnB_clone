612e0a2be094168ce0bb35610d71dc275dabe41f
ClaraCastaD <1980@holbertonschool.com>
2020-11-03 04:03:31 +0000

    Fixing error with self

--- models/review.py
+++ models/review.py
@@ -5,6 +5,6 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """class review that inherites from BaseModel"""
-    self.place_id = ""
-    self.user_id =  ""
-    self.text = ""
+    place_id = ""
+    user_id =  ""
+    text = ""
