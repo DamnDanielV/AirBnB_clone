547e0b711378d3b9eddd97de4bc264258f42ec35
DamnDanielV <1821@holbertonschool.com>
2020-11-04 23:47:58 +0000

    fixed pep8

--- models/review.py
+++ models/review.py
@@ -6,5 +6,5 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """class review that inherites from BaseModel"""
     place_id = ""
-    user_id =  ""
+    user_id = ""
     text = ""
