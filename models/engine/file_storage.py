a4cc0e4cc9a4f960f0b6007039487328bab2d0f7
d4n137-coder <62660407+d4n137-coder@users.noreply.github.com>
2020-11-04 09:03:17 -0500

    fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,9 +2,14 @@
 """ Module file storage """
 
 import json
+from datetime import datetime
+from models.base_model import BaseModel
 from models.user import User
-from ..base_model import BaseModel
-to_dict = BaseModel.to_dict
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage():
