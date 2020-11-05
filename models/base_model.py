ca529d66a444e9802bceb67b5ce5effe83a2439a
DamnDanielV <1821@holbertonschool.com>
2020-11-05 01:19:57 +0000

    fixed

--- models/base_model.py
+++ models/base_model.py
@@ -7,23 +7,11 @@ from datetime import datetime
 
 class BaseModel:
     """class base model"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """initialize an instance"""
-        if kwargs:
-            self.__dict__ = kwargs
-            try:
-                self.__dict__['updated_at'] = datetime.strptime(
-                    self.__dict__['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                self.__dict__['created_at'] = datetime.strptime(
-                    self.__dict__['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            except Exception as e:
-                pass
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-            # models.storage.save()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string representation"""
@@ -33,13 +21,18 @@ class BaseModel:
     def save(self):
         """updates the current date time"""
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing keys/values"""
-        return {
-                "id": self.id,
-                "__class__": self.__class__.__name__,
-                "updated_at": self.updated_at.isoformat(),
-                "created_at": self.created_at.isoformat()
-                }
+        r_dict = self.__dict__.copy()
+        for key in r_dict:
+            if key == 'created_at':
+                r_dict[key] = self.created_at.isoformat()
+            elif key == 'updated_at':
+                r_dict[key] = self.updated_at.isoformat()
+            elif key == 'id':
+                r_dict[key] = self.id
+
+        r_dict['__class__'] = self.__class__.__name__
+
+        return r_dict
